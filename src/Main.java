3553c28a3c557c9b9f624a426a4e4279cdce5611
Maria Mihali <mihali_maria90@yahoo.com>
2020-04-14 12:07:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Hello");
+        System.out.println("Maria");
+
+        int x=10, y=5;
+        int rez=x+y;
+        System.out.println(rez);
+
+        double a=10,b=3;
+        double rez1=a/b;
+        System.out.println(rez1);
+
+        x=-5;y=8; int z=6;
+        int rez2=x+y*z;
+        System.out.println(rez2);
+
+        x=55; y=9;
+        int rez3=(x+y)%y;
+        System.out.println(rez3);
+
+        float x1=20f, y1=3f, z1=5f,t1=8f;
+        float rez4=x1+-y1*z1/t1;
+        System.out.println(rez4);
+
+        x=5; y=15; z=3; int u=2; int v=8;
+        int rez5=x+y/z*u-v%z;
+        System.out.println(rez5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
